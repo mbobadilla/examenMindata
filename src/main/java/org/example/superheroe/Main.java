e759108b480a3d7a9cecde603eeeb2766f6bd591
Mario Bobadilla <mbobadilla@gmail.com>
2021-06-09 21:23:52 -0300

    Inclusion Swagger

--- src/main/java/org/example/superheroe/Main.java
+++ src/main/java/org/example/superheroe/Main.java
@@ -1,12 +1,12 @@
 package org.example.superheroe;
 
-import com.mangofactory.swagger.plugin.EnableSwagger;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableSwagger
 @EnableCaching
 public class Main {
     public static void main(String[] args) {
